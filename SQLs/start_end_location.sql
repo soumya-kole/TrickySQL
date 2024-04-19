7e3de009d51e6841189ddc69fd69e15d1e462651
soumya <soumyakole@gmail.com>
2024-04-18 20:06:33 -0500

    rough

--- SQLs/start_end_location.sql
+++ SQLs/start_end_location.sql
@@ -21,18 +21,15 @@ INSERT INTO travel_data (customer, start_loc, end_loc) VALUES
 with cte as (
  select 
 	coalesce (t1.customer,t2.customer) as customer,
-	t1.start_loc as s1,
-	t1.end_loc as e1,
-	t2.start_loc as s2,
-	t2.end_loc as e2
+	t1.start_loc as start_loc,
+	t2.end_loc as end_loc
 from travel_data t1 full outer join travel_data t2
 on t1.customer = t2.customer
 and t1.start_loc = t2.end_loc
 where t1.start_loc is null or t2.end_loc is null)
 select 
 	customer,
-	max(s1) as start_loc,
-	max(e2) as end_loc
+	max(start_loc) as start_loc,
+	max(end_loc) as end_loc
 from cte	
 group by 1
-;
