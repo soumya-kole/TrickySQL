bac59f853a5f28c4299f50d3917cebd0966958ff
Soumyabrata Kole <soumyakole@gmail.com>
2024-06-25 22:12:55 -0500

    Update rough.py

--- SQLs/rough.py
+++ SQLs/rough.py
@@ -1,1 +1,3 @@
 
+Within the first week, I started understanding customer requirements and ensuring simple deliveries. By the end of the first month, I began guiding other team members in technical deliveries. Within two months, I took charge of all complex delivery and debugging activities while continuously engaging, guiding, and educating team members. I improved the quality of the code delivered, surpassing the standards before I joined, within three months. By the fourth month, I started enhancing centralized frameworks like DDQ.
+
