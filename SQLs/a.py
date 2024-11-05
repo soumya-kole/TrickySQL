20d2e9972d87dc3f4e26db0418c8b8112f331511
Soumyabrata Kole <soumyakole@gmail.com>
2024-11-05 12:52:09 -0600

    Update a.py

--- SQLs/a.py
+++ SQLs/a.py
@@ -1,1 +1,7 @@
+The text in the image reads:
 
+Contract360
+
+This application hosted on Palantir Foundry, typically referred to as “Rx Term Manager,” provides the capability for business analysts to map contracts to adjudicated prescriptions.
+
+There are five major components required to accurately “Match” a contract rate to a prescription
