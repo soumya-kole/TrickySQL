42f3694e14e5a091e11a5b9088042e9d669f410a
Soumyabrata Kole <soumyakole@gmail.com>
2025-02-04 15:00:14 -0600

    Update a.py

--- SQLs/a.py
+++ SQLs/a.py
@@ -1,1 +1,9 @@
 
+git checkout branch-name
+rm -rf .git
+git init
+git remote add origin https://github.com/your-user/your-repo.git
+git add .
+git commit -m "Reset branch-name to latest state"
+git branch -M branch-name
+git push --force origin branch-name
